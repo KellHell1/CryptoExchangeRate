fb26370cd5fa92c5b693467d552ee12997c101ac
Nikolay <mykolagalanix@gmail.com>
2024-03-14 19:52:04 +0200

    разработка функционала сервиса

--- src/Controller/RateHistoryController.php
+++ src/Controller/RateHistoryController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use App\Service\RateHistoryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RateHistoryController extends AbstractController
 {
-    #[Route('/rate/history', name: 'app_rate_history', methods: ['GET'])]
-    public function getRateHistory(): JsonResponse
+    public function __construct(
+        private RateHistoryService $rateHistoryService,
+    )
     {
-        return $this->json([
-            'message' => 'Rate History',
-            'path' => 'src/Controller/RateHistoryController.php',
+        // TODO: переделать через ДИ
+        $this->rateHistoryService = new RateHistoryService();
+    }
+
+    #[Route('/rate/history', name: 'app_rate_history', methods: ['GET'])]
+    public function getRateHistory(int $currencyPairId, \DateTime $dateFrom, \DateTime $dateTo): JsonResponse
+    {
+        $history = $this->rateHistoryService->getRateHistory($currencyPairId, $dateFrom, $dateTo);
+
+        return new JsonResponse([
+            $history
         ]);
     }
 }
--- src/Service/RateHistoryService.php
+++ src/Service/RateHistoryService.php
@@ -3,23 +3,73 @@
 namespace App\Service;
 
 use App\Entity\CurrencyPair;
+use App\Entity\RateHistory;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class RateHistoryService
 {
-    public function getRateByApi(CurrencyPair $currencyPair): array
-    {
-        return [];
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private HttpClientInterface $client,
+        private string $getRateUrl,
+        private string $apiKey,
+        private array $query,
+    ) {
+        $this->getRateUrl = $_ENV['COIN_API_GET_RATE_URL'];
+        $this->apiKey = $_ENV['COIN_API_KEY'];
+        $this->query = [
+            'apikey' => $this->apiKey
+        ];
     }
 
 
-    public function saveRate(CurrencyPair $currencyPair): array
+    private function sendRequest($url, $options = ['query' => $this->query], $method = 'GET')
     {
-        return [];
+        try {
+            $response = $this->client->request(
+                $method,
+                $url . $this->apiKey,
+                $options
+            );
+        } catch (TransportExceptionInterface $e) {
+            // пишем в логи о ошибке
+        }
+
+        return isset($response) ? $response->toArray() : [];
+    }
+
+    private function getRateByApi(CurrencyPair $currencyPair): array
+    {
+        $currencyFrom = $currencyPair->getCurrencyFrom()->getCode();
+        $currencyTo = $currencyPair->getCurrencyTo()->getCode();
+
+        return $this->sendRequest($this->getRateUrl . "/$currencyFrom" . "/$currencyTo");
     }
 
 
-    public function getRateHistory(CurrencyPair $currencyPair, \DateTime $dateFrom, \DateTime $dateTo): array
+    private function saveRateHistory(CurrencyPair $currencyPair, array $response): array
     {
-        return [];
+        $rateHistory = new RateHistory();
+
+        $rateHistory->setRate($response['rate']);
+        $rateHistory->setCurrencyPair($currencyPair);
+        $rateHistory->setDatetime($response['time']);
+
+        $this->entityManager->persist($rateHistory);
+        $this->entityManager->flush($rateHistory);
+
+        return ['status' => 'ok'];
+    }
+
+
+    public function getRateHistory(int $currencyPairId, \DateTime $dateFrom, \DateTime $dateTo): array
+    {
+        $currencyPair = $this->entityManager->getRepository(CurrencyPair::class)->find($currencyPairId)
+            ?? throw new NotFoundHttpException();
+
+        return $this->entityManager->getRepository(RateHistory::class)->findBy(['currencyFrom' => $currencyPair->getCurrencyFrom(), 'currencyTo' => $currencyPair->getCurrencyTo()]);
     }
 }
