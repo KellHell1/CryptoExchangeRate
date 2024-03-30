016ca126b0d75b3c47dbca2433f10f623d065d9a
Nikolay <mykolagalanix@gmail.com>
2024-03-30 16:54:18 +0200

    рефакторинг. удалил лишнее

--- src/Repository/CurrencyPairRepository.php
+++ src/Repository/CurrencyPairRepository.php
@@ -20,29 +20,4 @@ class CurrencyPairRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, CurrencyPair::class);
     }
-
-//    /**
-//     * @return CurrencyPair[] Returns an array of CurrencyPair objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?CurrencyPair
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
--- src/Repository/RateHistoryRepository.php
+++ src/Repository/RateHistoryRepository.php
@@ -42,29 +42,4 @@ class RateHistoryRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult();
     }
-
-//    /**
-//     * @return RateHistory[] Returns an array of RateHistory objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('r.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?RateHistory
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
